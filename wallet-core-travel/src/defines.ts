27dff71044b8388e45e10dd14fbab47fee191b47
zhangshengjie <z.cejay@gmail.com>
2022-07-26 10:54:23 +0800

    MVP CLI - draft 0

---
+++ wallet-core-travel/src/defines.ts
@@ -0,0 +1,25 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Autor: z.cejay@gmail.com
+ * @Date: 2022-07-26 10:08:48
+ * @LastEditors: cejay
+ * @LastEditTime: 2022-07-26 10:13:02
+ */
+
+
+export const AddressZero = '0x0000000000000000000000000000000000000000';
+
+/**
+ * entry point contract address. singleton contract.(test address)
+ */
+export const EntryPointAddress = '0xeC97dc3Ef34b876caA9CC3E7EF91DE0D66cd689F';
+
+/**
+ * create2factory address defined in eip-2470.
+ */
+export const Create2Factory = '0xce0042B868300000d44A59004Da54A005ffdcf9f';
+
+
+
+
--- wallet-core-travel/src/entity/userOperation.ts
+++ wallet-core-travel/src/entity/userOperation.ts
@@ -4,10 +4,11 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2022-07-25 10:53:52
  * @LastEditors: cejay
- * @LastEditTime: 2022-07-25 11:37:18
+ * @LastEditTime: 2022-07-26 10:36:46
  */
 
-import { AbiCoder } from "ethers/lib/utils";
+import { BigNumberish } from 'ethers'
+import { BytesLike } from '@ethersproject/bytes'
 
 /**
  * @link https://github.com/eth-infinitism/account-abstraction/blob/develop/contracts/UserOperation.sol    
@@ -20,66 +21,47 @@ class UserOperation {
     /**
      * @param nonce unique value the sender uses to verify it is not a replay.
      */
-    nonce: number = 0;
+    nonce: BigNumberish = 0;
     /**
      * @param initCode if set, the account contract will be created by this constructor
      */
-    initCode: string = '0x';
+    initCode: BytesLike = '0x';
     /**
      * @param callData the method call to execute on this account.
      */
-    callData: string = '0x';
+    callData: BytesLike = '0x';
     /**
      * @param callGas gas used for validateUserOp and validatePaymasterUserOp
      */
-    callGas: number = 0;
+    callGas: BigNumberish = 0;
     /**
      * @param verificationGas gas not calculated by the handleOps method, but added to the gas paid. Covers batch overhead.
      */
-    verificationGas: number = 0;
+    verificationGas: BigNumberish = 0;
     /**
      * @param preVerificationGas gas not calculated by the handleOps method, but added to the gas paid. Covers batch overhead.
      */
-    preVerificationGas: number = 0;
+    preVerificationGas: BigNumberish = 0;
     /**
      * @param maxFeePerGas same as EIP-1559 gas parameter
      */
-    maxFeePerGas: number = 0;
+    maxFeePerGas: BigNumberish = 0;
     /**
      * @param maxPriorityFeePerGas same as EIP-1559 gas parameter
      */
-    maxPriorityFeePerGas: number = 0;
+    maxPriorityFeePerGas: BigNumberish = 0;
     /**
      * @param paymaster if set, the paymaster will pay for the transaction instead of the sender
      */
-    paymaster: string = '0x';
+    paymaster: BytesLike = '0x';
     /**
      * @param paymasterData extra data used by the paymaster for validation
      */
-    paymasterData: string = '0x';
+    paymasterData: BytesLike = '0x';
     /**
      * @param signature sender-verified signature over the entire request, the EntryPoint address and the chain ID.
      */
-    signature: string = '0x';
-
-    hash() {
-        /*
-        keccak256(abi.encode(
-                        userOp.getSender(),
-                        userOp.nonce,
-                        keccak256(userOp.initCode),
-                        keccak256(userOp.callData),
-                        userOp.callGas,
-                        userOp.verificationGas,
-                        userOp.preVerificationGas,
-                        userOp.maxFeePerGas,
-                        userOp.maxPriorityFeePerGas,
-                        userOp.paymaster
-                    ));
-         */
-        const abi = new AbiCoder();
-        // #todo
-    }
+    signature: BytesLike = '0x';
 }
 
 
